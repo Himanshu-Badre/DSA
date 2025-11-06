b81696e12e771f99450add6ac1e12cfc11951d2f
Himanshu Badre <144579622+Himanshu-Badre@users.noreply.github.com>
2025-11-06 22:53:33 +0530

    Time: 3 ms (68.82%) | Memory: 44 MB (48.19%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String lowerCase= s.toLowerCase();
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+           if(!Character.isLetterOrDigit(lowerCase.charAt(left))){
+            left++;
+            continue;
+           }
+           if(!Character.isLetterOrDigit(lowerCase.charAt(right))){
+            right--;
+            continue;
+           }
+           if(lowerCase.charAt(left)!=lowerCase.charAt(right)){
+            return false;
+           }
+           left++;
+           right--;
+        }       
+        return true;
+    }
+}
